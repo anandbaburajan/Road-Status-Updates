b1785a71fed7f1152a140c8109e0af426190aea8
Anand Baburajan <anandbaburajan@gmail.com>
2019-08-10 19:59:09 +0530

    Added Bootstrap

--- add.php
+++ add.php
@@ -1,5 +1,11 @@
 <html>
+    <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+  </head>
 <body>
+<div class="container">
 
 <?php
 
@@ -22,14 +28,16 @@ file_put_contents('places.json', $newJsonString1);
 ?>
 
 
-<p>Status of <b><?php echo $to . " - " . $from . " route";?> </b> added</p>
-
-Status: <b><?php echo $status; ?></b>
-<br>As of:<b><?php echo $asof; ?></b>
-<br>Remarks: <b><?php echo $remarks; ?></b>
+<h2>Status of <?php echo $to . " - " . $from . " route";?> added</h2>
+<br>
+<h4>Status: <b><?php echo $status; ?></b></h4>
+<h4>As of: <b><?php echo $asof; ?></b></h4>
+<h4>Remarks: <b><?php echo $remarks; ?></b></h4>
 
 <br><br>
-<a href="https://www.gizmolead.com/keralaroutes/update.html">Update another route</a>
-<br><a href="https://www.gizmolead.com/keralaroutes/">Check a route</a>
+<a href="https://www.gizmolead.com/keralaroutes/update.html"><button type="button" class="btn btn-primary">Update another route</button></a>
+<br><br>
+<a href="https://www.gizmolead.com/keralaroutes/"><button type="button" class="btn btn-info">Check a route</button></a>
+</div>
 </body>
 </html>
